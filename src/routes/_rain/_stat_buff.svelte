67cdb9b55e1ea5c9fb4f5f64e227705dcf13935a
Eerik Saksi <saksi.eerik@gmail.com>
2021-07-18 14:39:44 +0100

    Add

--- src/routes/_rain/_stat_buff.svelte
+++ src/routes/_rain/_stat_buff.svelte
@@ -1,9 +1,18 @@
 <script>
-	//https://play.google.com/store/apps/details?id=com.statbuff
-	//https://apps.apple.com/ee/app/stat-buff/id1551529468
+	let technologies = [
+		['react.webp', 'svelte.webp', 'django.webp', 'jquery.webp'],
+		['materialui.webp', 'bootstrap.webp', 'tailwind.webp'],
+
+		['reactnative.webp', 'expo.webp'],
+
+		['apollo.webp', 'typegraphql.webp', 'postgraphile.webp'],
+		['postgres.webp', 'sequelize.webp', 'typeorm.webp', 'sqlalchemy.webp'],
+
+		['pandas.webp', 'scikit.webp', 'spotlight.webp', 'networkx.webp']
+	];
 </script>
 
-<div class="absolute w-full flex flex-col h-auto  justify-center items-center bottom-0 bg-blue-800" >
+<div class="absolute w-full flex flex-col h-auto  justify-center items-center bottom-0 bg-blue-800">
 	<div class="flex flex-col justify-center items-center p-8">
 		<p class="text-3xl md:text-5xl text-center">Passionate About Full Stack</p>
 		<p class="text-3xl md:text-5xl text-center">Passionate About Full Stack</p>
