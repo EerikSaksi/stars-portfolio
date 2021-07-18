c4c56458d1b9fe163146f4a18043d38d1ee75984
Eerik Saksi <saksi.eerik@gmail.com>
2021-07-18 14:52:29 +0100

    Added techs

--- src/routes/_rain/_stat_buff.svelte
+++ src/routes/_rain/_stat_buff.svelte
@@ -1,29 +1,25 @@
 <script>
 	let technologies = [
-		['react.webp', 'svelte.webp', 'django.webp', 'jquery.webp'],
+		['react.webp', 'svelte.webp', 'django.webp',  'reactnative.webp', 'expo.webp'],
 		['materialui.webp', 'bootstrap.webp', 'tailwind.webp'],
-
-		['reactnative.webp', 'expo.webp'],
-
 		['apollo.webp', 'typegraphql.webp', 'postgraphile.webp'],
 		['postgres.webp', 'sequelize.webp', 'typeorm.webp', 'sqlalchemy.webp'],
 
-		['pandas.webp', 'scikit.webp', 'spotlight.webp', 'networkx.webp']
+		['pandas.webp', 'scikit.webp', 'spotlight.webp', 'networkx.png']
 	];
 </script>
 
-<div class="absolute w-full flex flex-col h-auto  justify-center items-center bottom-0 bg-blue-800">
+<div class="absolute w-full flex flex-col h-auto justify-center items-center bottom-0 bg-blue-500">
 	<div class="flex flex-col justify-center items-center p-8">
-		<p class="text-3xl md:text-5xl text-center">Passionate About Full Stack</p>
-		<p class="text-3xl md:text-5xl text-center">Passionate About Full Stack</p>
+		{#each technologies as row}
+			<div class="flex flex-row ">
+				{#each row as technology}
+					<div class="shadow-lg rounded-full m-2 p-3" style = "background-color: rgba(255,255,255,0.7);" >
+						<img class="h-16" src={'techs/' + technology} alt="Technology" />
+					</div>
+				{/each}
+			</div>
+		{/each}
 	</div>
 </div>
 
-<style>
-	p {
-		color: white;
-	}
-	img {
-		margin: 16px;
-	}
-</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,6 @@
 <script>
 	import Stars from './_stars/_stars.svelte';
 	import Cloud from './_rain/_cloud.svelte';
-	import Rain from './_rain/_rain.svelte';
 </script>
 
 <div class="relative">
